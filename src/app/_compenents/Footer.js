df56eb1181901b79f3e67a216bb512dd97ccb9f6
yanieznaio <juliette.or@icloud.com>
2023-08-07 21:26:57 +0200

    changes on project

--- src/app/_compenents/Footer.js
+++ src/app/_compenents/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='w-[92%] md:w-3/6 max-w-[700px] flex flex-col gap-2 md:flex-row items-center justify-between mt-28 mx-auto text-[14px] font-medium text-dark-light2 pb-12'>
       <p>
-Créé par Juliette Orth.</p>
+Développé par Juliette Orth.</p>
       <div className='flex gap-4'>
         <a href="https://github.com/yanieznaio" target="_blank "className='cursor-pointer hover:text-dark-light'>Github</a>
         <a href="/cv.pdf" target="_blank" className='cursor-pointer hover:text-dark-light'>Cv</a>
