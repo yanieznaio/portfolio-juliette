9fbbb042299307266264d416e844629c7bfb3e44
yanieznaio <juliette.or@icloud.com>
2023-08-09 19:06:24 +0200

    change project animation

--- src/app/_compenents/Project.js
+++ src/app/_compenents/Project.js
@@ -6,15 +6,19 @@ import {BsArrowRight} from "react-icons/bs"
 
 const variants = {
     hidden: {
-        y: "100vh"
+        y: 500,
+        opacity: 0,
     },
     visible: {
         
         y:0,
+        opacity: 1,
         transition: {
             type: "spring",
-            duration: 0.7,
-            delay: 0.2,
+            damping: 30,
+            stiffness: 100,
+          
+         
         }
      
     }
@@ -24,12 +28,12 @@ const Project = ({project}) => {
 
    
   return (
-    <motion.div  whileInView={() => setIsInView(true)} viewport={{once: true}}>
+    <motion.div  whileInView={() => setIsInView(true)} viewport={{once: true, amount: 0.5}}>
         <motion.div variants={variants} initial="hidden" animate={isInView && "visible"} className="flex flex-col gap-8 text-dark-light">
         <div className='w-5/6 m-auto flex flex-col gap-4'>
         <h2 className={`  font-bold text-[24px]`}>{project.name}</h2>
         <p className='text-dark-light2 text-[13px] md:text-[16px] leading-[22.4px] md:leading-[28.8px] font-normal'>{project.descrtiption}</p>
-        <a href={project.url} target="_blank" className='transition ease-in-out cursor-pointer flex items-center gap-2 text-dark-light2 hover:text-dark-light hover:ml-2 font-medium text-[16px] '><BsArrowRight/>voir une démo</a>
+        <motion.a initial={{x:0}} whileHover={{x:10}} transition={{type:"spring", damping:6, stiffness:300}} href={project.url} target="_blank" className='transition ease-in-out cursor-pointer flex items-center gap-2 text-dark-light2 hover:text-dark-light font-medium text-[16px] '><BsArrowRight/>voir une démo</motion.a>
         </div>
         <div className='w-700 h-500'>
         <Image src={project.img} alt={project.name}   height={600} width={600} objectFit="cover"  className='rounded-[20px]' unoptimized={true}/>
