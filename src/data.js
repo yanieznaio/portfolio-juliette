fb9d2a11ed56e166138f44903f2ba47cf2eb2a9c
yanieznaio <juliette.or@icloud.com>
2023-08-10 16:49:30 +0200

    add last project

--- src/data.js
+++ src/data.js
@@ -5,6 +5,7 @@ import goodfit from '../assets/goodfitClone.png'
 import chessgame from '../assets/chessGame.png'
 import pokedex from '../assets/pokedex.png'
 import beerTrack from "../assets/beerTrack.png"
+import animateflowers from "../assets/animate-flower.png"
 export const projects = [
     {
         name: "Pomodoro",
@@ -14,6 +15,14 @@ export const projects = [
         techno: ['react']
   
     },
+    {
+        name: "Animate Flowers",
+        descrtiption: "J'ai créé ce site pour pouvoir suivre et mesurer mes compétences en animation. Chaque page sera donc un projet différent pour pouvoir mesurer ma progression. J'ai choisi un thème unique (les fleurs) afin de garder une certaine cohérence sur le site. ",
+        img: animateflowers,
+        url: "https://animate-flowers.vercel.app/",
+        techno:['Next']
+    },
+
     {
         name: "Reedo",
         descrtiption: "Reedo est une bibliothèque numérique créée avec React. Elle vous permet d'ajouter ou de modifier des livres à votre profil. Vous pouvez également visualiser la bibliothèque d'autres utilisateurs. Vous pouvez retrouver le code d'une version de ce projet incluant une partie serveur sur mon GitHub. Actuellement, ce lien mène vers une démo du front uniquement accessible sur CodeSandbox.(en production)"        ,
