75bc397a78dce7688455475bcdef6fe68b4bb98c
yanieznaio <juliette.or@icloud.com>
2024-06-01 11:33:08 +0200

    modif

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,5 +1,5 @@
 "use client"
-import {motion} from "framer-motion"
+import { motion } from "framer-motion"
 import Hero from "../_compenents/Hero";
 import background from "../../../assets/contactBackground.webp"
 import Image from "next/image";
@@ -20,8 +20,8 @@ const text = {
       bounce: 0.20,
       duration: 0.5
     }
-    
-  
+
+
   }
 }
 const img = {
@@ -41,24 +41,20 @@ const img = {
 }
 export default function Page() {
 
-  const SERVICE_ID = "service_pr9wpfd";
-  const TEMPLATE_ID = "template_jfjfcee";
-  const USER_ID = "MXgsOBq3hq3wAQg2f"
-   
   const form = useRef();
 
   const handleOnSubmit = (e) => {
 
 
     e.preventDefault();
-    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID,  form.current, USER_ID)
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, USER_ID)
       .then((result) => {
         Swal.fire({
           icon: 'success',
           title: 'Message envoyé ! Je vous recontacterai dans les plus brefs délais.'
         })
       }, (error) => {
-    
+
         Swal.fire({
           icon: 'error',
           title: "Une erreur s'est produite",
@@ -70,55 +66,55 @@ export default function Page() {
 
 
   return (
-    
 
-<div className="flex min-h-screen flex-col items-center">
 
-    <Hero background={background}/>
-    <div className='w-[60%] md:w-3/6 max-w-[600px] flex flex-col gap-5 -mt-16 '>
-    <motion.div variants={img} initial="hidden" animate="visible">
-    <Image alt="emoji" src={emoji} width={72} height={72} className="z-50"></Image>
-    </motion.div>
+    <div className="flex flex-col items-center min-h-screen">
 
-    <motion.h1 variants={text} initial="hidden" animate="visible" className={` font-extrabold text-[35px] md:text-[48px] text-dark-light leading-[48px]`}>Contactez moi.</motion.h1>
-    <motion.p variants={text} initial="hidden" animate="visible" className='text-[14px] md:text-[15px] leading-[22.4px] md:leading-[25.6px]  text-dark-light2'>
-    Vous souhaitez me contacter ? Vous recherchez une développeuse front end ?
-    Vous pouvez dès maintenant m’envoyer un message grâce à ce formulaire de contact. Je vous répondrez dans les plus brefs délais.
-    </motion.p>
+      <Hero background={background} />
+      <div className='w-[60%] md:w-3/6 max-w-[600px] flex flex-col gap-5 -mt-16 '>
+        <motion.div variants={img} initial="hidden" animate="visible">
+          <Image alt="emoji" src={emoji} width={72} height={72} className="z-50"></Image>
+        </motion.div>
 
-    <motion.form ref={form} onSubmit={handleOnSubmit} variants={text} initial="hidden" animate="visible" className="flex flex-col gap-2 w-full  m-auto">
-    <div className="flex flex-wrap -mx-3 ">
-      <div className="w-full md:w-1/2 px-3 mb-2 md:mb-0">
-    
-        <input type="text" 
-           required
-           name="user_name"
-        placeholder="Nom" 
-        className="outline-none bg-light-gray2 py-2 rounded-md px-4">
+        <motion.h1 variants={text} initial="hidden" animate="visible" className={` font-extrabold text-[35px] md:text-[48px] text-dark-light leading-[48px]`}>Contactez moi.</motion.h1>
+        <motion.p variants={text} initial="hidden" animate="visible" className='text-[14px] md:text-[15px] leading-[22.4px] md:leading-[25.6px]  text-dark-light2'>
+          Vous souhaitez me contacter ? Vous recherchez une développeuse front end ?
+          Vous pouvez dès maintenant m’envoyer un message grâce à ce formulaire de contact. Je vous répondrez dans les plus brefs délais.
+        </motion.p>
 
-        </input>
+        <motion.form ref={form} onSubmit={handleOnSubmit} variants={text} initial="hidden" animate="visible" className="flex flex-col w-full gap-2 m-auto">
+          <div className="flex flex-wrap -mx-3 ">
+            <div className="w-full px-3 mb-2 md:w-1/2 md:mb-0">
+
+              <input type="text"
+                required
+                name="user_name"
+                placeholder="Nom"
+                className="px-4 py-2 rounded-md outline-none bg-light-gray2">
+
+              </input>
+            </div>
+            <div className="w-full px-3 md:w-1/2 ">
+
+              <input type="text"
+                required
+                name="email"
+                placeholder="Email" className="px-4 py-2 rounded-md outline-none bg-light-gray2"></input>
+            </div>
+          </div>
+          <textarea placeholder="message"
+            required
+            name="Message"
+            className="w-full h-48 p-2 rounded-md outline-none resize-none bg-light-gray2"></textarea>
+          <button type="submit" className="transition ease-in-out w-full  bg-dark-light text-white rounded-md p-3 hover:opacity-80 font-semibold text-[15px]">Envoyer le message</button>
+        </motion.form>
+
+
       </div>
-      <div className="w-full md:w-1/2 px-3 ">
 
-        <input type="text" 
-           required
-           name="email" 
-        placeholder="Email" className="outline-none bg-light-gray2 py-2 rounded-md px-4"></input>
-      </div>
     </div>
-    <textarea placeholder="message" 
-       required
-       name="Message"
-    className="outline-none p-2 h-48 w-full bg-light-gray2 rounded-md resize-none"></textarea>
-    <button type="submit" className="transition ease-in-out w-full  bg-dark-light text-white rounded-md p-3 hover:opacity-80 font-semibold text-[15px]">Envoyer le message</button>
-    </motion.form>
 
 
-</div>
-
-</div>
-
-  
   )
 }
 
